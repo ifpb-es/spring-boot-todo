3879aa8841d3a7febaaebf5397ae2e648405583e
Jaindson Valentim Santana <jaindson.santana.ext@synchro.com.br>
2025-08-04 20:02:33 -0300

    Minor refactor.

--- src/main/java/br/edu/ifpb/es/daw/todo/rest/advice/GlobalExceptionHandler.java
+++ src/main/java/br/edu/ifpb/es/daw/todo/rest/advice/GlobalExceptionHandler.java
@@ -54,8 +54,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	private ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
 		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError) error).getField();
+		ex.getBindingResult().getFieldErrors().forEach((error) -> {
+			String fieldName = error.getField();
 			String errorMessage = error.getDefaultMessage();
 			errors.put(fieldName, errorMessage);
 		});
