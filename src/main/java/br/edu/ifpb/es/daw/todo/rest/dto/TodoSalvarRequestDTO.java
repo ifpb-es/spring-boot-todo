35df1b9f055f1acdff102fcbe95958d315e21746
Jaindson Valentim Santana <jaindson.santana.ext@synchro.com.br>
2025-08-05 15:03:27 -0300

    Adicionando documentação dos DTOs.

--- src/main/java/br/edu/ifpb/es/daw/todo/rest/dto/TodoSalvarRequestDTO.java
+++ src/main/java/br/edu/ifpb/es/daw/todo/rest/dto/TodoSalvarRequestDTO.java
@@ -1,3 +1,7 @@
 package br.edu.ifpb.es.daw.todo.rest.dto;
 
-public record TodoSalvarRequestDTO(String descrição) { }
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record TodoSalvarRequestDTO(@Schema(description = "Descrição da tarefa.")
+                                   String descrição) {
+}
