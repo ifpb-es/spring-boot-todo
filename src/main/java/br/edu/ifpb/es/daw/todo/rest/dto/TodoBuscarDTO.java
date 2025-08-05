263ae50f5a73c7e3fece379289be0e81ca1dd476
Jaindson Valentim Santana <jaindson.santana.ext@synchro.com.br>
2025-08-05 15:05:13 -0300

    Adicionando documentação dos DTOs.

--- src/main/java/br/edu/ifpb/es/daw/todo/rest/dto/TodoBuscarDTO.java
+++ src/main/java/br/edu/ifpb/es/daw/todo/rest/dto/TodoBuscarDTO.java
@@ -1,8 +1,14 @@
 package br.edu.ifpb.es.daw.todo.rest.dto;
 
-public record TodoBuscarDTO(String descrição,
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record TodoBuscarDTO(@Schema(description = "Descrição da tarefa.")
+							String descrição,
+							@Schema(description = "'true' caso a tarefa de interesse seja concluída, 'false' caso contrário.")
 							Boolean concluído,
+							@Schema(description = "Número da página a ser retornada na paginação. Começa com zero.")
 							Integer númeroPágina,
+							@Schema(description = "Quantidade de registros a serem retornados por página.")
 							Integer tamanhoPágina) {
 
 	public TodoBuscarDTO {
--- src/main/java/br/edu/ifpb/es/daw/todo/rest/dto/TodoSalvarRequestDTO.java
+++ src/main/java/br/edu/ifpb/es/daw/todo/rest/dto/TodoSalvarRequestDTO.java
@@ -1,6 +1,11 @@
 package br.edu.ifpb.es.daw.todo.rest.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record TodoSalvarRequestDTO(@NotBlank @Size(min = 5, max = 255) String descrição) { }
+public record TodoSalvarRequestDTO(@Schema(description = "Descrição da tarefa.")
+                                   @NotBlank
+                                   @Size(min = 5, max = 255)
+                                   String descrição) {
+}
