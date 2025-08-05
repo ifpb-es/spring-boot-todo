1d15289e412ec48842344c588828ccdd308653d1
Jaindson Valentim Santana <jaindson.santana.ext@synchro.com.br>
2025-08-05 15:27:50 -0300

    Adicionando documentação dos DTOs.

--- src/main/java/br/edu/ifpb/es/daw/todo/rest/dto/UsuarioResponseDTO.java
+++ src/main/java/br/edu/ifpb/es/daw/todo/rest/dto/UsuarioResponseDTO.java
@@ -1,8 +1,15 @@
 package br.edu.ifpb.es.daw.todo.rest.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.util.UUID;
 
 @Builder
-public record UsuarioResponseDTO(UUID lookupId, String nome, String email) { }
+public record UsuarioResponseDTO(@Schema(description = "Lookup ID da tarefa.")
+                                 UUID lookupId,
+                                 @Schema(description = "Nome do usuário.")
+                                 String nome,
+                                 @Schema(description = "E-mail do usuário (login).")
+                                 String email) {
+}
