acc6e4a9cf8aa975f473810fbc22991a10693922
Jaindson Valentim Santana <jaindson.santana.ext@synchro.com.br>
2025-08-09 12:59:38 -0300

    Adicionando CORS

---
+++ src/main/java/br/edu/ifpb/es/daw/todo/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package br.edu.ifpb.es.daw.todo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allow-credentials}")
+    private Boolean allowCredentials;
+
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
+
+    @Value("${app.cors.allowed-methods}")
+    private String allowedMethods;
+
+    @Value("${app.cors.allowed-headers}")
+    private String allowedHeaders;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all paths
+                .allowedOrigins(splitByComma(allowedOrigins))
+                .allowedMethods(splitByComma(allowedMethods))
+                .allowedHeaders(splitByComma(allowedHeaders))
+                .allowCredentials(allowCredentials)
+                .maxAge(3600);
+    }
+
+    private String[] splitByComma(String value) {
+        return value.split(",");
+    }
+}
