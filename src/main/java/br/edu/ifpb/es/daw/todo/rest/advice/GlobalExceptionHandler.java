ca9cfb9451f782e39a489527a6ab24aae5e6bdd4
Jaindson Valentim Santana <jaindson.santana.ext@synchro.com.br>
2025-08-17 16:11:34 -0300

    Minor refactor.

--- src/main/java/br/edu/ifpb/es/daw/todo/rest/advice/GlobalExceptionHandler.java
+++ src/main/java/br/edu/ifpb/es/daw/todo/rest/advice/GlobalExceptionHandler.java
@@ -1,25 +1,23 @@
 package br.edu.ifpb.es.daw.todo.rest.advice;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.net.URI;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-
+import br.edu.ifpb.es.daw.todo.exception.EstadoInválidoException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import br.edu.ifpb.es.daw.todo.exception.EstadoInválidoException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -48,7 +46,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(
 			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-		return ResponseEntity.ofNullable(handleMethodArgumentNotValidException(ex));
+		return ResponseEntity.of(handleMethodArgumentNotValidException(ex)).build();
 	}
 
 	private ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
