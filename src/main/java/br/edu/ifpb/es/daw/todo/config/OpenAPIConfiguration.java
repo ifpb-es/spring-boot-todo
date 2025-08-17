ebc108ae3403c74841f5d01e77195e46b486bd48
Jaindson Valentim Santana <jaindson.santana.ext@synchro.com.br>
2025-08-17 15:13:57 -0300

    Minor refactor.

--- src/main/java/br/edu/ifpb/es/daw/todo/config/OpenAPIConfiguration.java
+++ src/main/java/br/edu/ifpb/es/daw/todo/config/OpenAPIConfiguration.java
@@ -1,19 +1,15 @@
 package br.edu.ifpb.es.daw.todo.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.extensions.Extension;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 
 import java.util.Arrays;
 
